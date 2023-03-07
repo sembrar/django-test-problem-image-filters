05092d0be59d08c89c422d522300c9d7d0d51b76
sembrar <sagarpj95@gmail.com>
2023-03-07 23:47:07 +0530

    Added Login and Logout functionality
    On branch main
    Changes to be committed:
     modified:   users/urls.py
        Used django's class built in Login and Logout class based views for login and logout, they have all the functionality required
     modified:   image_filters_assignment/settings.py
        Added a setting for log-in-success-redirect url
     modified:   users/templates/users/register.html
        The link to login page is updated.
     new file:   users/templates/users/login.html
     new file:   users/templates/users/logout.html

--- image_filters_assignment/settings.py
+++ image_filters_assignment/settings.py
@@ -124,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'image-filters-home'  # otherwise, after log in, django redirects to /accounts/profile.html
