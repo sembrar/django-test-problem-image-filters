1f2d74ead0bec753469034a5437782311cbe4129
sembrar <sagarpj95@gmail.com>
2023-03-08 01:06:05 +0530

    Created UploadedImages model class
    This model class corresponds to a table with the same name in the database
    It has two fields,
     1. An image field for the uploaded image
     2. A reference to the user (called foreign key from another table) who uploaded that image
    
    On branch main
    Changes to be committed:
     modified:   image_filters/admin.py
     modified:   image_filters/models.py
     new file:   image_filters/migrations/0001_initial.py
        migrations is a way, django handles changes in database table definitions without losing any data or relationships between them

--- image_filters/admin.py
+++ image_filters/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import UploadedImages
 
-# Register your models here.
+
+admin.site.register(UploadedImages)  # so that the database table of UploadedImages is visible in the admin page
--- image_filters/models.py
+++ image_filters/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UploadedImages(models.Model):
+    image = models.ImageField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    # in the above, one user can own many images (one to many relationship), and one image can have at most one owner
+    # "on_delete = CASCADE" makes sure that if a user is deleted, all their images are deleted as well
