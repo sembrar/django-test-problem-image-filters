fb422a0481bbad33358af9f8c49136ca1a55e2b2
sembrar <sagarpj95@gmail.com>
2023-03-09 06:28:17 +0530

    Created FilteredImage model class
    On branch main
    Changes to be committed:
     modified:   image_filters/models.py
     new file:   image_filters/migrations/0002_filteredimage.py

--- image_filters/models.py
+++ image_filters/models.py
@@ -7,3 +7,10 @@ class UploadedImages(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     # in the above, one user can own many images (one to many relationship), and one image can have at most one owner
     # "on_delete = CASCADE" makes sure that if a user is deleted, all their images are deleted as well
+
+
+class FilteredImage(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    file_name = models.CharField(max_length=200)
+    filter_name = models.CharField(max_length=100)
+    original_img = models.ForeignKey(UploadedImages, on_delete=models.CASCADE)
